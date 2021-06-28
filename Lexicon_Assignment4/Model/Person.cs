04ba6f4b0df95c9a63394f1abe81134f5ea78f7d
Daniella Noren <daniellanoren@live.se>
2021-06-28 13:55:28 +0200

    Created person-class

---
+++ Lexicon_Assignment4/Model/Person.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Lexicon_Assignment4.Model
+{
+    public class Person
+    {
+        public int PersonId { get { return personId; } }
+
+        private readonly int personId;
+        public string FirstName
+        {
+            get { return firstName; }
+            set
+            {
+                CheckNullOrEmptyString(value);
+                firstName = value;
+            }
+        }
+
+        public string LastName
+        {
+            get { return lastName; }
+            set
+            {
+                CheckNullOrEmptyString(value);
+                lastName = value;
+            }
+        }
+
+        private string firstName;
+        private string lastName;
+
+        public Person(int personId, string firstName, string lastName)
+        {
+            this.personId = personId;
+            this.FirstName = firstName;
+            this.LastName = lastName;
+        }
+
+        public void CheckNullOrEmptyString(string str)
+        {
+            if (string.IsNullOrEmpty(str))
+                throw new NullReferenceException();
+        }
+    }
+}
