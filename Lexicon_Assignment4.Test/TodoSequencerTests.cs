24be940432e5dc9d39df5b4dc4910097487dc434
Fredrik Ericson <fredrikericson@outlook.com>
2021-06-29 08:17:25 +0200

    TodoSequencer class and test

---
+++ Lexicon_Assignment4.Test/TodoSequencerTests.cs
@@ -0,0 +1,38 @@
+﻿using Lexicon_Assignment4.Data;
+using Xunit;
+
+namespace Lexicon_Assignment4.Test
+{
+
+    public class TodoSequencerTests
+    {
+        [Fact]
+        public void IdCounterWorks()
+        {
+            // Arrange
+            TodoSequencer.reset();
+
+            //Act
+            int before = TodoSequencer.nextTodoId();
+            int result = TodoSequencer.nextTodoId();
+
+            //Assert
+            Assert.True(before < result);
+        }
+
+        [Fact]
+        public void ResetWorks()
+        {
+            // Arrange
+            TodoSequencer.reset();
+
+            //Act
+            int before = TodoSequencer.nextTodoId();
+            TodoSequencer.reset();
+            int result = TodoSequencer.nextTodoId();
+
+            //Assert
+            Assert.True(before == result);
+        }
+    }
+}
